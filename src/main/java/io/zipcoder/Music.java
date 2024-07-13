655ad1935c403438d523f9ffb03f6f21a8830aa6
coreyeross25 <coreyeross12@gmail.com>
2024-07-13 10:56:27 -0400

    2/2 test passed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,23 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
-    private String[] playList;
+    private final String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        ArrayList<String> musicSelections = new ArrayList<String>(Arrays.asList(this.playList));
+
+        int forwardCt = Math.abs(musicSelections.indexOf(selection) - startIndex);
+
+        int backwardCt = Math.abs((musicSelections.size() + startIndex) - musicSelections.lastIndexOf(selection));
+
+        return Math.min(forwardCt, backwardCt);
     }
 }
